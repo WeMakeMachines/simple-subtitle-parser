c29f322c857b6a8923dabacca5ce90d40ca2edf1
Franco Speziali <franco@wemakemachines.com>
2020-03-09 19:48:08 +0100

    Updated SrtParser to use new filter method

--- source/Parsers/SrtParser/index.ts
+++ source/Parsers/SrtParser/index.ts
@@ -4,8 +4,9 @@ import Parser from '../Parser/index';
 export default class SrtParser extends Parser {
 	parse(string: string): Cue[] {
 		const multiline = this.stringToMultiline(string);
-		const rawCueContents = this.multilineToRawCueContent(multiline);
+		const allRawCueData = this.multilineToRawCueContent(multiline);
+		const rawCueData = this.dropInvalidCueData(allRawCueData);
 
-		return this.parseCueData(rawCueContents);
+		return this.parseCueData(rawCueData);
 	}
 }
