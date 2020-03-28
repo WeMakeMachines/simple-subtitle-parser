b6cf382015c222056802cd0842fbf8a2fa4c488a
Franco Speziali <franco@wemakemachines.com>
2020-03-28 19:13:56 +0100

    Output dir change lib -> dist

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from 'rollup-plugin-typescript';
 export default {
     input: 'source/index.ts',
     output: {
-        file: 'lib/subtitle-parsing-tool.js',
+        file: 'dist/subtitle-parsing-tool.js',
         format: 'cjs'
     },
     plugins: [
