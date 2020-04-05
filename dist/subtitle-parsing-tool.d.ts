4999bff9a6febc48e8a0878b31c63d2054ab9eda
Franco Speziali <franco@wemakemachines.com>
2020-04-05 23:53:23 +0200

    Added typings

---
+++ dist/subtitle-parsing-tool.d.ts
@@ -0,0 +1,12 @@
+interface Cue {
+    sequence: number;
+    startTime: number;
+    endTime: number;
+    text: string[];
+}
+declare enum Formats {
+    Srt = "SRT",
+    WebVtt = "WEBVTT"
+}
+declare const parser: (format: Formats, string: string) => Promise<Cue[]>;
+export { parser };
